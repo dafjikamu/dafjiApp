daa29d97f119fcdf57797d06472804bf256d5ad7
dafjikamu <153999254+dafjikamu@users.noreply.github.com>
2025-07-10 20:16:51 -0400

    Create select-wrapper.tsx

---
+++ ui/components/component-library/select-wrapper/select-wrapper.tsx
@@ -0,0 +1,30 @@
+import React, { forwardRef, useState, useRef } from "react";
+import classnames from "classnames";
+import { Box, type PolymorphicRef, BoxProps } from "../box";
+import { Popover, PopoverPosition } from "../popover";
+
+export const SelectWrapper = forwardRef(
+  <C extends React.ElementType = "div">(
+    {
+      className = "",
+      defaultValue,
+      value,
+      onValueChange,
+      placeholder,
+      isDanger,
+      isDisabled,
+      isOpen: controlledOpen,
+      onOpenChange: controlledOnChange,
+      isMultiSelect: multiSelectMode = false,
+
+      
+    },
+    ref
+  ) => {
+    
+    
+
+   
+  
+  }
+);
